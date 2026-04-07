bc9af9e1bb54fd80a2c99016dee23bde43298cd0
tatajae <shintaaaaagustinaaaa@gmail.com>
2026-04-07 19:12:06 +0700

    pesan update Anda

--- database/blog (2).sql
+++ database/blog (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2026 at 04:11 AM
+-- Generation Time: Apr 07, 2026 at 02:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,10 +42,12 @@ CREATE TABLE `artikel` (
 --
 
 INSERT INTO `artikel` (`id_artikel`, `judul`, `isi`, `tanggal`, `id_user`, `id_kategori`, `gambar`) VALUES
-(7, 'Jeong Jaehyun ', 'dikabarkan jeong jaehyun selesai wamil pada 3 mei 20206', '2026-04-24', 1, 2, 'img_69d3d14ddaef8.jfif'),
-(52, 'jamal', 'Nama Panggung: Jaehyun (재현)\r\nNama Lahir: Jeong Jae-hyun (정재현), secara legal diganti menjadi Jeong Yun-o (정윤오)\r\nTempat, Tanggal Lahir: Seoul, Korea Selatan, 14 Februari 1997\r\nZodiak: Aquarius\r\nTinggi Badan: 180 cm\r\nGolongan Darah: A\r\nSub-unit: NCT U, NCT 127\r\nInstagram: @jeongjaehyun', '2026-04-09', 1, 1, 'img_69d3d1402c2d3.jfif'),
-(61, 'jamal', 'Debut di bawah SM Entertainment tahun 2016, ia anggota aktif NCT U dan NCT 127. Dikenal dengan vokal lembut dan visual memukau, Jaehyun fasih berbahasa Inggris karena pernah tinggal di Connecticut, AS, selama 4 tahun.', '2026-04-06', 2, 3, 'img_69d3c8e449b2f.jfif'),
-(65, 'jeong', 'Jaehyun NCT (Jeong Yun-o) adalah penyanyi, penari, dan aktor asal Korea Selatan yang lahir pada 14 Februari 1997 di Seoul.', '2026-04-06', 3, 2, 'jamal.jfif');
+(65, 'Tips Belajar Pemrograman Web untuk Pemula', 'Belajar pemrograman web merupakan langkah awal yang baik bagi siapa saja yang ingin terjun ke dunia teknologi. Dengan memahami dasar-dasar pemrograman web, seseorang dapat membuat website sederhana hingga aplikasi web yang kompleks.\r\n\r\nLangkah pertama yang perlu dipelajari adalah HTML. HTML digunakan untuk membuat struktur dasar dari sebuah halaman web seperti judul, paragraf, gambar, dan link.\r\n\r\nSetelah memahami HTML, langkah berikutnya adalah mempelajari CSS. CSS berfungsi untuk mempercantik tampilan website, seperti mengatur warna, ukuran font, tata letak halaman, dan membuat desain menjadi lebih menarik.\r\n\r\nSelanjutnya, pelajari JavaScript agar website menjadi lebih interaktif. Dengan JavaScript, kita bisa membuat fitur seperti tombol interaktif, validasi form, dan animasi pada halaman web.\r\n\r\nJika sudah memahami ketiga dasar tersebut, kita dapat melanjutkan dengan mempelajari bahasa pemrograman backend seperti PHP dan menggunakan database MySQL untuk menyimpan data.\r\n\r\nDengan latihan yang konsisten dan membuat proyek kecil seperti blog sederhana, kemampuan pemrograman web akan berkembang dengan cepat.', '2026-04-07', 3, 2, 'img_69d4e85295157.jfif'),
+(66, 'Belajar Dasar HTML untuk Pemula', 'HTML merupakan bahasa dasar yang digunakan untuk membuat struktur halaman website. Dengan HTML, kita dapat membuat berbagai elemen seperti judul, paragraf, gambar, dan tautan.\r\n\r\nBagi pemula yang ingin belajar membuat website, memahami HTML adalah langkah pertama yang sangat penting. HTML memiliki struktur sederhana yang mudah dipelajari.\r\n\r\nSetelah memahami HTML, biasanya pengembang web akan melanjutkan belajar CSS untuk mengatur tampilan halaman agar lebih menarik.', '2026-04-07', 3, 4, '1775562048_html.jfif'),
+(67, 'Cara Mengatur Waktu Belajar dengan Baik', 'Mengatur waktu belajar sangat penting agar materi yang dipelajari dapat dipahami dengan baik. Banyak pelajar merasa kesulitan belajar karena tidak memiliki jadwal yang jelas.\r\n\r\nSalah satu cara yang efektif adalah membuat jadwal belajar harian. Dengan jadwal tersebut, kita dapat menentukan waktu khusus untuk belajar tanpa terganggu aktivitas lainnya.\r\n\r\nSelain itu, jangan lupa untuk memberikan waktu istirahat agar pikiran tetap segar.', '2026-04-07', 3, 3, '1775562219_download (6).jfif'),
+(68, 'Manfaat Olahraga bagi Kesehatan Tubuh', 'Olahraga merupakan aktivitas yang sangat penting untuk menjaga kesehatan tubuh. Dengan berolahraga secara rutin, tubuh akan menjadi lebih bugar dan kuat.\r\n\r\nSelain menjaga kesehatan fisik, olahraga juga dapat membantu mengurangi stres dan meningkatkan suasana hati.\r\n\r\nBeberapa olahraga ringan seperti jogging, bersepeda, dan senam dapat dilakukan secara rutin setiap hari.', '2026-04-07', 3, 2, '1775562380_download (7).jfif'),
+(69, 'Tips Menjadi Lebih Produktif Setiap Hari', 'Produktivitas merupakan kemampuan seseorang untuk menyelesaikan pekerjaan secara efektif dan efisien. Untuk meningkatkan produktivitas, kita perlu mengatur waktu dengan baik.\r\n\r\nSalah satu cara yang dapat dilakukan adalah membuat daftar tugas harian. Dengan daftar tersebut, kita dapat mengetahui pekerjaan apa saja yang harus diselesaikan.\r\n\r\nSelain itu, hindari gangguan seperti penggunaan media sosial secara berlebihan.', '2026-04-07', 3, 1, '1775562518_download (8).jfif'),
+(70, 'Pentingnya Membaca Buku Setiap Hari', 'Membaca buku merupakan kegiatan yang memberikan banyak manfaat bagi kehidupan seseorang. Dengan membaca, kita dapat menambah wawasan dan pengetahuan baru.\r\n\r\nSelain itu, membaca juga dapat meningkatkan kemampuan berpikir kritis serta memperluas cara pandang terhadap berbagai hal.\r\n\r\nMeluangkan waktu sekitar 15–30 menit setiap hari untuk membaca buku dapat memberikan dampak positif bagi perkembangan diri.', '2026-04-07', 3, 6, '1775562602_download (9).jfif');
 
 -- --------------------------------------------------------
 
@@ -66,7 +68,24 @@ CREATE TABLE `artikel_tag` (
 INSERT INTO `artikel_tag` (`id`, `id_artikel`, `id_tag`) VALUES
 (1, 62, 1),
 (2, 63, 1),
-(6, 65, 1);
+(6, 65, 1),
+(7, 66, 1),
+(8, 66, 4),
+(9, 66, 6),
+(10, 66, 7),
+(11, 67, 3),
+(12, 67, 5),
+(13, 67, 8),
+(14, 67, 9),
+(15, 68, 3),
+(16, 68, 10),
+(17, 68, 11),
+(18, 69, 3),
+(19, 69, 8),
+(20, 69, 9),
+(21, 70, 5),
+(22, 70, 9),
+(23, 70, 11);
 
 -- --------------------------------------------------------
 
@@ -84,10 +103,11 @@ CREATE TABLE `kategori` (
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
-(1, 'jepang'),
-(2, 'korea'),
-(3, 'cina'),
-(4, 'thailand');
+(1, 'Produktivitas'),
+(2, 'Kesehatan'),
+(3, 'Pendidikan'),
+(4, 'Teknologi'),
+(6, 'Lifestyle');
 
 -- --------------------------------------------------------
 
@@ -110,15 +130,8 @@ CREATE TABLE `komentar` (
 --
 
 INSERT INTO `komentar` (`id_komentar`, `id_user`, `id_artikel`, `nama`, `komentar`, `tanggal`, `status`) VALUES
-(1, 5, 7, NULL, 'jamal ganteng banget', '2026-04-07', 'approved'),
-(2, 0, 0, 'ichi', 'jamal', '2026-04-07', 'pending'),
-(3, 0, 0, 'ichi', 'jamal', '2026-04-07', 'pending'),
-(4, 0, 0, 'ichi', 'jamal', '2026-04-07', 'pending'),
-(5, 0, 0, 'ichi', 'jamal', '2026-04-07', 'pending'),
-(6, 0, 0, 'ichi', 'tes', '2026-04-07', 'pending'),
-(7, 5, 0, NULL, 'p', '2026-04-07', 'pending'),
-(8, 5, 0, NULL, 'tes', '2026-04-07', 'pending'),
-(9, 5, 0, NULL, 'tes', '2026-04-07', 'pending');
+(17, 5, 70, 'ichi', 'makasih kaa tipsnya', '2026-04-07', 'approved'),
+(18, 10, 68, 'titi', 'ayoo kita olah raga bareng kaa', '2026-04-07', 'approved');
 
 -- --------------------------------------------------------
 
@@ -136,8 +149,16 @@ CREATE TABLE `tag` (
 --
 
 INSERT INTO `tag` (`id_tag`, `nama_tag`) VALUES
-(1, 'kpop'),
-(3, 'drama');
+(1, 'Tutorial'),
+(3, 'Tips'),
+(4, 'Pemula'),
+(5, 'Belajar'),
+(6, 'Coding'),
+(7, 'Web'),
+(8, 'Produktivitas'),
+(9, 'Motivasi'),
+(10, 'Kesehatan'),
+(11, 'Lifestyle');
 
 -- --------------------------------------------------------
 
@@ -212,31 +233,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `artikel`
 --
 ALTER TABLE `artikel`
-  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- AUTO_INCREMENT for table `artikel_tag`
 --
 ALTER TABLE `artikel_tag`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `komentar`
 --
 ALTER TABLE `komentar`
-  MODIFY `id_komentar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_komentar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tag`
 --
 ALTER TABLE `tag`
-  MODIFY `id_tag` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_tag` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
